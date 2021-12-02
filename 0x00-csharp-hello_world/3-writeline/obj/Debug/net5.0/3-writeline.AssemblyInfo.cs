264b0655cdbf043f70b9cdd6da5a415d3f0438dd
Rode1o <dumirola.dr@gmail.com>
2021-12-02 18:42:20 -0500

    Swithch to Net 5

--- 0x00-csharp-hello_world/3-writeline/obj/Debug/net5.0/3-writeline.AssemblyInfo.cs
+++ 0x00-csharp-hello_world/3-writeline/obj/Debug/net5.0/3-writeline.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("4-print_integer")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("4-print_integer")]
-[assembly: System.Reflection.AssemblyTitleAttribute("4-print_integer")]
+[assembly: System.Reflection.AssemblyProductAttribute("3-writeline")]
+[assembly: System.Reflection.AssemblyTitleAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
