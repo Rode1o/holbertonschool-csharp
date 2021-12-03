0ab03c3ec39b538ce1c3676d8ad5baa66ad874c5
Rode1o <dumirola.dr@gmail.com>
2021-12-02 20:34:11 -0500

    print a string stored in the variable str 3 times, followed by its first 9 characters.

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string str = "Holberton School";
+        Console.WriteLine($"{str}{str}{str}\n{str.Substring(0, 9)}");
+    }
+}
