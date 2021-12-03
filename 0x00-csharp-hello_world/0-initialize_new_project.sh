7df4ec6b689ba9a9f3e4cc6b58f13620da918adb
Duvan R <77517122+Rode1o@users.noreply.github.com>
2021-12-03 12:10:10 -0500

    Delete 0x00-csharp-hello_world directory

--- 0x00-csharp-hello_world/0-initialize_new_project.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Initializes a new C# project inside a folder titled 0-new_project.
-dotnet new console -o 0-new_project
--- 0x00-csharp-hello_world/2-run_new_project.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Initializes, builds and runs a new C# project inside a folder titled 2-new_project.
-dotnet new console -o 2-new_project
-dotnet build 2-new_project
-dotnet run --project 2-new_project
