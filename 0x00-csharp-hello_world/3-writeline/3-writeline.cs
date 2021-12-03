3418be23f9693131c0ac810f326be881cc2d77b0
Rode1o <dumirola.dr@gmail.com>
2021-12-02 19:47:24 -0500

    .net 5 rebuild

---
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3_writeline
+{
+    class writeline
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
